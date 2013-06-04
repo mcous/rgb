5fd362db014bf3c390138dc3caf0ec3a577755a5
Michael Cousins <cousmj@gmail.com>
2013-06-04 17:53:52 -0500

    Wrote Makefile and added main program frame

---
+++ firmware/rgb.cpp
@@ -0,0 +1,23 @@
+// rgb
+// daisy-chainable lighting module
+// http://github.com/mcous/rgb
+//
+// copyright 2013 michael cousins
+// shared under the terms of the gpl v3 licence
+
+// includes
+#include "rgb.h"
+
+// defines
+
+
+int main(void) {
+
+    // OH MY GOD IT'S ENDLESS
+    for(;;) {
+
+    }
+
+    // the answer
+    return 42;
+}
