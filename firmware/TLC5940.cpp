b4e7f179b2ec69e5404f2e208bc42af8c521c752
Michael Cousins <cousmj@gmail.com>
2013-06-05 12:21:16 -0500

    Updated initDC function

--- firmware/TLC5940.cpp
+++ firmware/TLC5940.cpp
@@ -6,25 +6,38 @@
 TLC5940::TLC5940(void) {
     // set control pins to outputs and initial values
     // gsclk
-    DDR_GSLCK |= (1 << GSCLK);
-    PORT_GSLCK &= ~(1 << GSCLK);
+    DDR_GSLCK |= (1 << GSCLK_PIN);
+    PORT_GSLCK &= ~(1 << GSCLK_PIN);
     // sclk
-    DDR_SCLK |= (1 << SCLK);
-    PORT_SCLK &= ~(1 << SCLK);
+    DDR_SCLK |= (1 << SCLK_PIN);
+    PORT_SCLK &= ~(1 << SCLK_PIN);
     // dcprg
-    DDR_DCPRG |= (1 << DCPRG);
-    PORT_DCPRG &= ~(1 << DCPRG);
+    DDR_DCPRG |= (1 << DCPRG_PIN);
+    PORT_DCPRG &= ~(1 << DCPRG_PIN);
     // vprg
-    DDR_VPRG |= (1 << VPRG);
-    PORT_VPRG |= (1 << VPRG);
+    DDR_VPRG |= (1 << VPRG_PIN);
+    PORT_VPRG |= (1 << VPRG_PIN);
     // xlat
-    DDR_XLAT |= (1 << XLAT);
-    PORT_XLAT &= ~(1 << XLAT);
+    DDR_XLAT |= (1 << XLAT_PIN);
+    PORT_XLAT &= ~(1 << XLAT_PIN);
     // blank
-    DDR_BLANK |= (1 << BLANK);
-    PORT_BLANK |= (1 << BLANK);
+    DDR_BLANK |= (1 << BLANK_PIN);
+    PORT_BLANK |= (1 << BLANK_PIN);
 }
 
 void TLC5940::initDC(void) {
-    
+    // set dcprg to 1 (don't use eeprom data)
+    PORT_DCPRG |= (1 << DCPRG_PIN);
+    // set vprg to 1 (program dc data)
+    PORT_VPRG |= (1 << VPRG_PIN);
+    // set serial data to low (setting dc to 0)
+    PORT_MOSI &= ~(1 << MOSI_PIN)
+    // pulse the serial clock 96 times to write in dc data
+    for (uint8_t i=0; i<96; i++) {
+        PORT_SCLK |= (1 << SCLK_PIN);
+        PORT_SCLK &= ~(1 << SCLK_PIN);
+    }
+    // pulse xlat to latch the data
+    PORT_XLAT |= (1 << XLAT_PIN);
+    PORT_XLAT &= ~(1 << XLAT_PIN);
 }
--- firmware/TLC5940.h
+++ firmware/TLC5940.h
@@ -7,27 +7,31 @@
 // greyscale clock - B0
 #define DDR_GSCLK  DDRB
 #define PORT_GSCLK PORTB
-#define GSCLK      0
+#define GSCLK_PIN  0
 // serial clock - B5
 #define DDR_SCLK  DDRB
 #define PORT_SCLK PORTB
-#define SCLK      5
+#define SCLK_PIN  5
 // dc programming select - D4
 #define DDR_DCPRG  DDRD
 #define PORT_DCPRG PORTD
-#define DCPRG      4
+#define DCPRG_PIN  4
 // programming select - D5
 #define DDR_VPRG  DDRD
 #define PORT_VPRG PORTD
-#define VPRG      5
+#define VPRG_PIN  5
 // latch - B2
 #define DDR_XLAT  DDRB
 #define PORT_XLAT PORTB
-#define XLAT      2
+#define XLAT_PIN  2
 // blank outputs - B1
 #define DDR_BLANK  DDRB
 #define PORT_BLANK PORTB
-#define BLANK      1
+#define BLANK_PIN  1
+// serial data master out slave in - B3
+#define DDR_MOSI  DDRB
+#define PORT_MOSI PORTB
+#define MOSI_PIN  3
 
 
 class TLC5940 {
