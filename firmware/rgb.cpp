46f1f7a408649e22cacf7724b0288622caedbd1e
Michael Cousins <cousmj@gmail.com>
2013-06-06 16:06:10 -0500

    CYLONS!!!!!

--- firmware/rgb.cpp
+++ firmware/rgb.cpp
@@ -16,7 +16,7 @@
 int main (void) {
     // set dc values
     for (uint8_t i=0; i<15; i++) {
-        driver.setDC(i, 10);
+        driver.setDC(i, 1);
     }
     // set some initial values for the leds
     for (uint8_t i=0; i<15; i++) {
@@ -46,24 +46,82 @@ int main (void) {
     // initialize dot correction on the led driver
     driver.init();
 
+    /*
     double a = 0;
     uint16_t red;
     uint16_t grn;
     uint16_t blu;
+    */
+
+    /*
+    uint16_t val[3];
+    val[0] = 500;
+    val[1] = 0;
+    val[2] = 0;
+    uint8_t dec = 0;
+    uint8_t inc = dec + 1;
+    uint16_t count = 0;
+    */
+
+    uint16_t count = 0;
+    uint8_t last = 0;
+    uint8_t curr = 0;
+    int8_t dir = 1;
 
     // OH MY GOD IT'S ENDLESS
     for(;;) {
+
+        // cylon stuff
+        if (count > 9) {
+            count = 0;
+            last = curr;
+            if (last <=2) {
+                dir = 1;
+            }
+            else if (last >= 12) {
+                dir = -1;
+            }
+            curr += 3*dir;
+        }
+        driver.setGS(last, 0);
+        driver.setGS(curr, 500);
+        driver.refreshGS();
+        count++;
+
+        // two-color-combo stuff
+        /*
+        if (count >= 499) {
+            count = 0;
+            dec++;
+            if (dec > 2) {
+                dec = 0;
+            }
+            inc = dec + 1;
+            if (inc == 3) {
+                inc = 0;
+            }
+        }
+        val[dec]-=1;
+        val[inc]+=1;
+        rgb(val[0], val[1], val[2]);
+        driver.refreshGS();
+        count++;
+        */
+
+        // sine wave stuff
+        /*
         if (a >= 6.28) {
             a = 0;
         }
 
         red = (uint16_t)(1000 * (1 + sin(a)));
-        grn = (uint16_t)(1000 * (1 + sin(a + (1*3.14/3.0))));
-        blu = (uint16_t)(1000 * (1 + sin(a + (2*3.14/3.0))));
+        grn = (uint16_t)(1000 * (1 + sin(a + (3.14/2.0))));
+        blu = (uint16_t)(1000 * (1 + sin(a + (3.14))));
         
         rgb(red, grn, blu);
         driver.refreshGS();
         a+=0.01;
+        */
     }
 
     // the answer
